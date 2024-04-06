d12d4932e0f09e8ea5fbfaf624bcfbfc395ea892
sagurathod <sagurathod@gmail.com>
2024-04-06 09:37:56 +0530

    menu_bar_code

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { HttpService } from '../servies/http.service';
 
 @Component({
   selector: 'app-menu-bar',
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
 })
 export class MenuBarComponent {
 
+  continentList : string [] = [];
+
+  currencyList : string [] = [];
+
+  @ViewChild('closeBtn') closeBtn! : ElementRef<any>;
+
+  @Output() emitAction = new EventEmitter<any>();
+
+  constructor(private http: HttpService){
+
+  }
+
+  ngOnInit(){
+    this.getContinent();
+    this.getCurrency();
+  }
+
+  getContinent(){
+    const endPoint = "continent";
+    this.http.getData(endPoint).subscribe((resp:any)=>{
+      this.continentList = resp;
+      console.log(this.continentList);
+    })
+  }
+
+  getCurrency(){
+    const endPoint = "currency";
+    this.http.getData(endPoint).subscribe((resp:any)=>{
+      this.currencyList = resp;
+      console.log(this.currencyList);
+    })
+  }
+
+  ngAfterViewInit() {
+    console.log("Close Btn", this.closeBtn.nativeElement.click());
+  }
+
+  closePopup() {
+    if (this.closeBtn && this.closeBtn.nativeElement) {
+      this.closeBtn.nativeElement.click();
+    }
+  }
 }
---
+++ src/app/servies/http.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  
+  baseUrl : string = "http://localhost:3000/";
+
+  constructor(private http:HttpClient) {
+
+  }
+
+  postData(endPoint : string, data : any){
+    const url = this.baseUrl + endPoint;
+    return this.http.post(url, data);
+  }
+
+  getData(endPoint : string){
+    const url = this.baseUrl + endPoint;
+    return this.http.get(url);
+  }
+
+  updateData(endPoint : string, data : any){
+    const url = this.baseUrl + endPoint;
+    return this.http.put(url, data);
+  }
+
+  deleteData(endPoint : string){
+    const url = this.baseUrl + endPoint;
+    return this.http.delete(url);
+  }
+}
