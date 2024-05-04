cf3a6a1d3fd98bb9e6ddef9fe1dcb01f76a04d86
sagurathod <sagurathod@gmail.com>
2024-05-04 16:34:47 +0530

    translate_functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { GoogleTranslateService } from './services/google-translate.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
 
-  
+  title = 'tabulator';
+
+  constructor(private translationService: GoogleTranslateService) { }
+
 }
